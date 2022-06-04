81b3e7296624f23de816d00e3775be95659b3b53
Anna Razina <vega_b@mail.ru>
2022-06-04 17:18:58 +0400

    basic concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,23 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  let d = (b**2)-(4*a*c);
+  let root
+  if (d<0) {
+    arr = [];
+  } else if (d===0) {
+    root = -b/(2*a);
+    arr.push(root);
+  } else if (d>0) {
+      root = (-b + Math.sqrt(d) )/(2*a);
+      arr.push(root);
+      root = (-b - Math.sqrt(d) )/(2*a);
+      arr.push(root);
+  } else {
+    arr = [];
+  }
+  
   // код для задачи №1 писать здесь
   return arr; // array
 }
