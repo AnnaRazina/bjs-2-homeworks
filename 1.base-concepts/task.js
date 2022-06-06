7650d6cc706a604348965c945f08c51853207c43
Anna Razina <vega_b@mail.ru>
2022-06-06 20:34:04 +0400

    тема-основные понятия js,решение задач 1,2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,9 @@
 "use strict"
-
+// код для задачи №1
 function solveEquation(a, b, c) {
   let arr = [];
   let d = (b**2)-(4*a*c);
-  let root
+  let root;
   if (d<0) {
     arr = [];
   } else if (d===0) {
@@ -16,36 +16,40 @@ function solveEquation(a, b, c) {
       arr.push(root);
   } else {
     arr = [];
-  }
+  };
   
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  return arr; 
+};
 
+
+// код для задачи №2 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   let loanBody = 0;
   loanBody = (parseInt(amount)) - (parseInt(contribution));
-  let loanTime = 0;
-  
-  let startCreditTime = new Date();
-  let andCreditTime = [date.getFullYear(), date.getMonth()];
-     if ((andCreditTime[0] - startCreditTime.getFullYear()) === 0) {
-        loanTime = (andCreditTime[1] + 1) - (startCreditTime.getMonth() + 1);
-     } else if ((andCreditTime[0] - startCreditTime.getFullYear()) === 1) {
-        loanTime = (12 - (startCreditTime.getMonth() + 1)) + (andCreditTime[1] + 1);
-     } else {
-        loanTime = (((andCreditTime[0] - startCreditTime.getFullYear()) - 1) * 12) + (12 - (startCreditTime.getMonth() + 1)) + (andCreditTime[1] + 1);
-     }
 
+      function calculationLoanTime (date) {
+          let loanTime = 0;
+          let startCreditTime = new Date();
+          let andCreditTime = [date.getFullYear(), date.getMonth()];
 
-  
-  
-     console.log(loanBody);
-     console.log(loanTime);
-     
-  // код для задачи №2 писать здесь
+          if ((andCreditTime[0] - startCreditTime.getFullYear()) === 0) {
+              loanTime = (andCreditTime[1] + 1) - (startCreditTime.getMonth() + 1);
+          } else if ((andCreditTime[0] - startCreditTime.getFullYear()) === 1) {
+              loanTime = (12 - (startCreditTime.getMonth() + 1)) + (andCreditTime[1] + 1);
+          } else {
+              loanTime = (((andCreditTime[0] - startCreditTime.getFullYear()) - 1) * 12) + (12 - (startCreditTime.getMonth() + 1)) + (andCreditTime[1] + 1);
+          };
 
-  return totalAmount;
-}
+          return loanTime;
+      };   
+  let loanTimeMonths = calculationLoanTime (date);
+
+  let P = (parseInt(percent)) / 100 / 12;
+  let MonthlyFee = loanBody * (P + (P / (((1 + P)**loanTimeMonths) - 1)));
+  totalAmount = +(MonthlyFee * loanTimeMonths).toFixed(2);
+ 
+  console.log(totalAmount);
+  return totalAmount;  
+};
