3829f051e7e8974475f1220c7cde0983b4d40a47
Anna Razina <vega_b@mail.ru>
2022-06-14 16:39:12 +0400

    Домашнее задание к лекции «Массивы». Задачи 1, 2.

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,24 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+  result = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 
   return result; // boolean
 }
 
+compareArrays([8, 9], [6]); // false, разные значения
+compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); // false, разные значения
+compareArrays([9, 2, 4, 8, 2], [9, 2, 4]); // false, разные значения
+compareArrays([1, 2, 3], [2, 3, 1]); // false, разные индексы, хотя и одинаковые значения
+compareArrays([8, 1, 2], [8, 1, 2]); // true
+
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+  resultArr = arr.filter((number) => number > 0 && number % 3 === 0)
+  .map((number) => number * 10);
 
   return resultArr; // array
 }
+
+advancedFilter([-1,6,-9,3]); // [60,30]
+advancedFilter([-10,-21,12,123]); // [120, 1230]
+advancedFilter([-1,-2]); // []
