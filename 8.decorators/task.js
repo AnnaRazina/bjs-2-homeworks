ae43c5cc43ea2792847c23fa5f358f4b592b2ea6
Anna Razina <vega_b@mail.ru>
2022-07-13 16:14:36 +0400

    Домашнее задание к лекции 8 «Декораторы»

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,60 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+      const hash = args.join(','); 
+      let objectInCache = cache.find((item) => item.hash === hash);
+      if (objectInCache !== undefined) { 
+          console.log("Из кэша: " + objectInCache.result); 
+          return "Из кэша: " + objectInCache.result;
+      } else if (!objectInCache) {
+          let result = func.call(this, ...args); 
+          cache.push({hash, result}) ; 
+            if (cache.length > 5) { 
+             cache.shift();
+            }
+          console.log("Вычисляем: " + result);
+          return "Вычисляем: " + result; 
+      } 
+  }
+  return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timerId;
+  let  firstCall = true;
+  function wrapper (...args) { 
+    if (firstCall !== true) {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+      func.call(this, ...args);
+      console.timeEnd("time2");
+      }, ms);
+      return timerId;
+    } else if (firstCall = true) {
+      timerId = func.call(this, ...args);
+      firstCall = false;
+      console.timeEnd("time1");
+      return firstCall;
+    }
+  }
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timerId;
+  let  firstCall = true;
+  function wrapper (...args) {  
+    if (firstCall !== true) {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+      func.call(this, ...args); 
+      }, ms);
+      this.count += 1;
+    } else if (firstCall = true) {
+      timerId = func.call(this, ...args);
+      firstCall = false;
+      this.count = 1;
+    }
+  }
+  return wrapper;
 }
